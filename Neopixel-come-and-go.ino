6aff9efd141b740edf3581ed7e5bd967d694596c
dnettoRaw <me@dnetto.dev>
2021-05-31 15:13:48 +0200

    rename file, and add gif

--- Neopixel-come-and-go.ino
+++ Neopixel-come-and-go.ino
@@ -7,15 +7,15 @@ Neopixel!
 // check this lib sources at https://github.com/outofambit/easy-neopixels
 #include <EasyNeoPixels.h>
 
-#define PIN 6		// pin for led's data 
-#define LEN 10		// how much Led's in strip
-#define TAIL 2		// for visual effect, this represent how much leds keep alive behind actual led on  
-#define DELAY 50 	// time in millisecounds
+#define PIN 6		    // pin for led's data 
+#define LEN 10		  // how much Led's in strip
+#define TAIL 3		  // for visual effect, this represent how much leds keep alive behind actual led on  
+#define DELAY 50 	  // time in millisecounds
 
 enum RGB {
-    RED 	= 250,	// betewn 0-255
-    GREEN 	= 0,	// betewn 0-255
-    BLUE	= 0	// betewn 0-255
+    RED 	  = 250,	// betewn 0-255
+    GREEN 	= 0,	  // betewn 0-255
+    BLUE	  = 0	    // betewn 0-255
 };
 
 
